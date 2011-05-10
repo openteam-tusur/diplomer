6e24fdb8c38b73cd3815185758f1b7d7daa2104b
Manin Iliy <bkmzvf@gmail.com>
2011-05-10 12:55:52 +0700

    #7737. Добавить поле квалификации в специальность

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -3,3 +3,19 @@ class Specialty < ActiveRecord::Base
   has_many :chair, :through => :chair_specialty
 end
 
+
+# == Schema Information
+#
+# Table name: specialties
+#
+#  id                     :integer         not null, primary key
+#  code                   :string(255)
+#  eng_code               :string(255)
+#  title                  :text
+#  eng_title              :text
+#  start_recruitment_year :string(255)
+#  end_recruitment_year   :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#
+
