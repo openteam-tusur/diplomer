4a483acefadaf66077ce1d39cd83d47abed3df54
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-05-18 14:41:30 +0700

    is_translated? -> translated?

--- spec/models/chair_spec.rb
+++ spec/models/chair_spec.rb
@@ -3,14 +3,14 @@
 require 'spec_helper'
 
 describe Chair do
-  it 'метод :is_translated? должен возвращать true если есть пустые поля с префиксом eng_' do
+  it 'метод :translated? должен возвращать true если есть пустые поля с префиксом eng_' do
     chair = Factory.create(:chair, :eng_abbr => nil)
-    chair.is_translated?.should be false
+    chair.translated?.should be false
   end
 
-  it 'метод :is_translated? должен возвращать true если нет пустых полей с префиксом eng_' do
+  it 'метод :translated? должен возвращать true если нет пустых полей с префиксом eng_' do
     chair = Factory.create(:chair)
-    chair.is_translated?.should be true
+    chair.translated?.should be true
   end
 end
 
--- vendor/plugins/has_translate/lib/has_translate/instance_methods.rb
+++ vendor/plugins/has_translate/lib/has_translate/instance_methods.rb
@@ -1,8 +1,10 @@
 module HasTranslate::InstanceMethods
-  def is_translated?
+  def translated?
     attributes_to_be_translated = attributes.delete_if { |attr| attr =~ /(id|created_at|updated_at|number)/ }.
                                              delete_if { |attr| attr !~ /eng_/ }
     attributes_to_be_translated.values.each { |value| return false if value.blank? }
     true
   end
+
+  alias :translated :translated?
 end
