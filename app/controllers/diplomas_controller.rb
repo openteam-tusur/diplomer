b9101e395ed14b099e6e1a4511d2621d52f5197f
Manin Iliy <bkmzvf@gmail.com>
2011-06-10 09:49:29 +0700

    Шаблон печати на бланке

--- app/controllers/diplomas_controller.rb
+++ app/controllers/diplomas_controller.rb
@@ -6,16 +6,10 @@ class DiplomasController < CrudController
     @student = Student.find(@diploma.student)
     @speciality = Speciality.find(@diploma.speciality)
     respond_to do |format|
+      format.html
       format.pdf do
         render :pdf => "#{@diploma}-#{@diploma.student}",
-               :page_size => 'A3',
-               :orientation => 'Landscape',
-               :dpi => '300',
-               :show_as_html  => params.has_key?(:debug),
-               :margin => {:top => '0',
-                           :right => '0',
-                           :bottom => '0',
-                           :left => '0'}
+          :stylesheets => ["print"]
       end
     end
   end
