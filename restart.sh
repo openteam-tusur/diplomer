503f82fb528878dbcd79525ac81f6d2d762e983e
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-06-16 15:18:49 +0700

    fix configs

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kill -QUIT `cat /var/run/unicorn-scientist.pid`
+kill -QUIT `cat /var/run/unicorn/unicorn-diplomer.pid`
 sleep 2
-cd /srv/young-scientist
+cd /srv/diplomer
 sudo -u www unicorn -c unicorn.rb -E production -D
 
