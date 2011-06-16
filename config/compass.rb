c33f9d0b2da9777bc941ca8221dae96382bb15b9
Evgeny Lapin <lev@openteam.ru>
2011-06-16 14:55:28 +0700

    fix compass config

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,8 @@
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
+
+require 'fancy-buttons'
+
 project_type = :rails
 
 # Set this to the root of your project when deployed:
@@ -15,3 +18,4 @@ http_path = "/"
 # line_comments = false
 
 preferred_syntax = :sass
+
