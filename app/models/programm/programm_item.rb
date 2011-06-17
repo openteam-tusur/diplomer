492a0a9408be8c78be4667cf826d80bf1f37b32b
Manin Iliy <bkmzvf@gmail.com>
2011-06-17 15:19:39 +0700

    сортировка programm_items

--- app/models/programm/programm_item.rb
+++ app/models/programm/programm_item.rb
@@ -7,6 +7,8 @@ class ProgrammItem < ActiveRecord::Base
 
   has_enum :grade, %w[satisfactorily good excellent passed], :scopes => true
 
+  default_scope :order => 'position DESC, created_at ASC'
+
   def to_s
     title
   end
