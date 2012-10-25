d18f501e4bf82c6437e14ae34a98e40977e42526
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-10-25 10:59:44 +0700

    Generate number for AcademicRecords

--- app/models/academic_record.rb
+++ app/models/academic_record.rb
@@ -7,7 +7,25 @@ class AcademicRecord < ActiveRecord::Base
 
   accepts_nested_attributes_for :student, :update_only => true
 
+  before_create :generate_number
+
   def to_s
     "Академическая справка №#{number}"
+  end
+
+  private
+
+  def generate_number
+    academic_records = self.class.where(:issued_on => (Date.today.beginning_of_year..Date.today.end_of_year), :faculty_id => self.faculty)
+
+    if last_academic_record = academic_records.last
+      number = last_academic_record.serial_number + 1
+    else
+      number = 1
+    end
+    formatted_number = sprintf("%05i",number)
+
+    self.number = "#{faculty.eng_abbr.upcase}#{I18n.l(Date.today, :format => '%y')}-#{formatted_number}"
+    self.serial_number = number
   end
 end
