a294142e8701adce4fa96518561becd9da31e708
tab <tukhfatullin@gmail.com>
2014-02-26 15:16:24 +0700

    compile assets on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,13 @@ require 'openteam/capistrano/recipes'
 
 set :default_stage, :production
 set :shared_children, fetch(:shared_children) + %w[config/sunspot.yml]
+
+namespace :compass do
+  desc "Compass assets precompile"
+  task :precompile do
+    run "cd #{deploy_to}/current && bundle exec compass compile -e production --force"
+  end
+end
+
+after "deploy", "compass:precompile"
+after "deploy:rollback", "compass:precompile"
