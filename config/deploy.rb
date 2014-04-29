502e7376014a42eaa5647352d94c4ee0b80991aa
tab <tukhfatullin@gmail.com>
2014-04-29 12:30:22 +0700

    Fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'openteam/capistrano/recipes'
 
 set :default_stage, :production
-set :shared_children, fetch(:shared_children) + %w[config/sunspot.yml]
+set :shared_children, fetch(:shared_children) + %w[config/config.yml config/sunspot.yml]
 
 namespace :compass do
   desc "Compass assets precompile"
