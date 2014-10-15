243ef914ac445f2fd89720fa7200651619045be2
tab <tukhfatullin@gmail.com>
2014-10-15 11:19:13 +0700

    Fix errbit settings

--- config/initializers/_read_configuration.rb
+++ config/initializers/_read_configuration.rb
@@ -1,1 +1,10 @@
+require 'configatron'
+
 configatron.configure_from_hash YAML.load_file(Rails.root.join('config', 'config.yml'))[Rails.env] rescue nil
+
+Airbrake.configure do |config|
+  config.api_key = configatron.errors.retrieve :key
+  config.host    = 'errors.openteam.ru'
+  config.port    = 80
+end
+
--- config/initializers/hoptoad.rb
+++
@@ -1,5 +0,0 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = configatron.hoptoad.retrieve :api_key, ENV['HOPTOAD_API_KEY']
-  config.host    = 'errors.openteam.ru'
-  config.port    = 80
-end if Rails.env.production?
